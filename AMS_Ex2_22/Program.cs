6e6cc7fef2a6643f36ae73d387ebfc490ea3db0a
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 15:31:32 -0300

    Add files via upload

---
+++ AMS_Ex2_22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AMS_Ex2_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double euro = 5.13;
+            double qtdEuro = 750;
+            double real = (qtdEuro * euro);
+
+            Console.WriteLine("");
+            Console.WriteLine($"O valor dessa quantia em euro convertida para o real é de: {real} reais");
+            Console.WriteLine("");
+        }
+    }
+}
